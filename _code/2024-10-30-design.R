6265e79e42419daf920e24116042b376ae61a1c7
nwickel <n.wickelmaier@iwm-tuebingen.de>
2024-11-05 17:08:34 +0100

    Try to insert figures

--- _code/2024-10-30-design.R
+++ _code/2024-10-30-design.R
@@ -195,6 +195,7 @@ text(3, datm$y[datm$interaction == "active" & datm$mediatype == "VR2"],
 #'
 
 #+
+knitr::include_graphics(paste0("../figures/", Sys.Date(), "_plot-1.png"))
 
 #' # Session info
 
